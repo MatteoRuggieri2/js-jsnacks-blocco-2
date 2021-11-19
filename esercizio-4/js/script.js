bee5ca17aef7d89f4fb8ff4df748e9206485491d
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-19 17:38:55 +0100

    Esercizio 3

---
+++ esercizio-4/js/script.js
@@ -0,0 +1,55 @@
+// ESERCIZIO
+
+// Crea un array vuoto e chiedi all’utente un numero
+// da inserire nell’array. Continua a chiedere i numeri
+// all’utente e a inserirli nell’array fino a quando
+// a somma degli elementi è minore di 50.
+
+let userNumbers = [];
+let somma = 0;
+
+
+
+
+
+while ( somma < 50 ) {
+    const userSingleNumber = parseInt(prompt('Inserisci un numero'));
+    console.log(userSingleNumber);
+    
+    if (!isNaN(userSingleNumber)){
+
+        userNumbers.push(userSingleNumber);
+        console.log(userNumbers);
+        
+        somma += arraySingleNumber
+        console.log(somma);
+        
+    }
+
+    
+}
+
+console.log('Il risultato è: ' + somma)
+
+
+
+
+
+
+// chiedo all'utente numeri finchè la somma non arriva a 50.
+
+// do {
+//     const userSingleNumber = parseInt(prompt('Inserisci un numero'));
+//     userNumbers.push(userSingleNumber);
+//     console.log(userNumbers);
+
+//     for (let i = 0; i < userNumbers.length; i++) {
+//         const arraySingleNumber = userNumbers[i];
+//         console.log(arraySingleNumber);
+//         somma += arraySingleNumber
+//     }
+
+// } while ( somma <= 50);
+
+
+// inserisco i numeri dentro l'array
